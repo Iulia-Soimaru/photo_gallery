b140e25802f730f9c6f0bc4b9f88657d52e363ea
Iulia Soimaru <iuliashoimaru@gmail.com>
2018-03-19 22:34:54 -0700

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'],
+          plugins: ['transform-class-properties']
+        }
+      }
+    ]
+  }
+}
