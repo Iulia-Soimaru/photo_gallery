bdbd629af5123f74c093e44806323b340403e93d
Iulia Soimaru <iuliashoimaru@gmail.com>
2018-03-20 23:41:15 -0700

    move Flickr class to it's own file

---
+++ src/flickr.js
@@ -0,0 +1,27 @@
+// https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=ebc7e0bc0d4517a47c79db1e1bc449b1&photoset_id=72157650609588450&format=json
+export default class Flickr {
+  constructor(callback) {
+    this.callback = callback;
+  }
+
+  static api() {
+    const restApi = 'https://api.flickr.com/services/rest/';
+    const method = 'flickr.photosets.getPhotos';
+    const apiKey = '8fbbeac186b0ccc02fac8f1d27ec85e8';
+    const photosetId = '72157646084694683';
+    const format = 'json';
+    const callback = 'jsonFlickrApi';
+    return restApi.concat(`?method=${method}&api_key=${apiKey}&photoset_id=${photosetId}&format=${format}&callback=${callback}`)
+  }
+
+  init = () => {
+    let elm = document.createElement('script');
+    elm.setAttribute('type', 'text/javascript');
+    elm.src = Flickr.api();
+    document.body.appendChild(elm);
+  }
+
+  jsonFlickrApi = (json) => {
+    this.callback(json);
+  }
+}
